ce4db7d3a42021dea516fa61140f7a0e042f2f68
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-02-25 15:01:29 +0100

    snack1

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+//SNACK1
+/* const num1 = parseInt(prompt('Inserisci il primo numero'));
+const num2 = parseInt(prompt('Inserisci il secondo numero'));
+
+if (isNaN(num1)||isNaN(num2)){
+    alert('Devi inserire due numeri!')
+} else if (num1==num2) {
+    document.getElementById('risultato').innerHTML= 'I numeri sono uguali';
+} else if (num1>num2) {
+    document.getElementById('risultato').innerHTML= 'Il primo numero è maggiore';
+} else {
+    document.getElementById('risultato').innerHTML= 'Il secondo numero è maggiore';
+} */
+
 //SNACK2
 /* const str1 = prompt('Inserisci la prima parola');
 const str2 = prompt('Inserisci la seconda parola');
@@ -6,7 +20,8 @@ if (str2.length>str1.length) {
     document.getElementById('risultato').innerHTML= str2 + ' ' + str1;
 } else {
     document.getElementById('risultato').innerHTML= str1 + ' ' + str2;
-} */
+} 
+*/
 
 //SNACK3
 /* let num1 = parseInt(prompt('Inserisci il primo numero'));
@@ -50,7 +65,8 @@ if (isNaN(num10)) {
     alert('Devi inserire un numero')
 }
 const result = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
-document.getElementById('risultato').innerHTML= result; */
+document.getElementById('risultato').innerHTML= result; 
+*/
 
 //SNACK4
 /* const invitati = ['Aldo', 'Luigi', 'Giuseppe', 'Gabriel', 'Mauro', 'Chiara'];
@@ -68,7 +84,8 @@ if (check) {
     check = false;
 } else {
     document.getElementById('risultato').innerHTML= 'Non sei stato invitato';
-} */
+} 
+*/
 
 //SNACK5
 /* const dispari = [];
@@ -80,10 +97,12 @@ for (let index = 1; index <= 6; index++) {
     }    
 }
 
-document.getElementById('risultato').innerHTML= dispari; */
+document.getElementById('risultato').innerHTML= dispari; 
+*/
 
 //SNACK6
-/* const num = prompt('Inserisci un numero di quattro cifre');
+/* 
+const num = prompt('Inserisci un numero di quattro cifre');
 let somma=0;
 if (num.length!=4||isNaN(num)){
     alert('Devi inserire quattro cifre!')
@@ -92,4 +111,5 @@ if (num.length!=4||isNaN(num)){
         somma+=parseInt(num.charAt(index));    
     }
     document.getElementById('risultato').innerHTML= somma;
-} */
+} 
+*/
