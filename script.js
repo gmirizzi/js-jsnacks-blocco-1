32cb2093edd3f534958e53175ce99efa130ff5c6
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-02-25 15:30:42 +0100

    migliorato snack3

--- script.js
+++ script.js
@@ -22,51 +22,19 @@ if (str2.length<str1.length) {
     document.getElementById('risultato').innerHTML= str1 + ' ' + str2;
 }  */
 
-
 //SNACK3
-/* let num1 = parseInt(prompt('Inserisci il primo numero'));
-if (isNaN(num1)) {
-    alert('Devi inserire un numero')
+/* let somma=0;
+for (let index = 1; index <= 10; index++) {
+    const num = parseInt(prompt('Inserisci un numero'));
+    if (isNaN(num)){
+        alert('Non è un numero!');
+        continue;
+    } else {
+        somma+=num;
+    }
 }
-let num2 = parseInt(prompt('Inserisci il secondo numero'));
-if (isNaN(num2)) {
-    alert('Devi inserire un numero')
-}
-let num3 = parseInt(prompt('Inserisci il terzo numero'));
-if (isNaN(num3)) {
-    alert('Devi inserire un numero')
-}
-let num4 = parseInt(prompt('Inserisci il quarto numero'));
-if (isNaN(num4)) {
-    alert('Devi inserire un numero')
-}
-let num5 = parseInt(prompt('Inserisci il quinto numero'));
-if (isNaN(num5)) {
-    alert('Devi inserire un numero')
-}
-let num6 = parseInt(prompt('Inserisci il sesto numero'));
-if (isNaN(num6)) {
-    alert('Devi inserire un numero')
-}
-let num7 = parseInt(prompt('Inserisci il settimo numero'));
-if (isNaN(num7)) {
-    alert('Devi inserire un numero')
-}
-let num8 = parseInt(prompt('Inserisci il ottavo numero'));
-if (isNaN(num8)) {
-    alert('Devi inserire un numero')
-}
-let num9 = parseInt(prompt('Inserisci il nono numero'));
-if (isNaN(num9)) {
-    alert('Devi inserire un numero')
-}
-let num10 = parseInt(prompt('Inserisci l\'ultimo numero'));
-if (isNaN(num10)) {
-    alert('Devi inserire un numero')
-}
-const result = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
-document.getElementById('risultato').innerHTML= result; 
-*/
+
+document.getElementById('risultato').innerHTML=somma; */
 
 //SNACK4
 /* const invitati = ['Aldo', 'Luigi', 'Giuseppe', 'Gabriel', 'Mauro', 'Chiara'];
